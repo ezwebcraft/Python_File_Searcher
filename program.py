ae40f51c0b3aa3d0dc71dd9d1d84b8c4996cf769
Elvin Ramirez <real8686@gmail.com>
2018-08-12 23:47:58 -0400

    fix bugs and missing items

--- program.py
+++ program.py
@@ -3,18 +3,23 @@ import os
 
 def main():
     print_header()
+
     folder = get_folder_from_user()
+
     if not folder:
         print("None searchable location")
         return
 
     text = get_search_text_from_user()
+
     if not text:
         print("We can't search for nothing!")
         return
 
-    search_folders(folder, text)
+    matches = search_folders(folder, text)
 
+    for m in matches:
+        print(m)
 
 def print_header():
     print('--------------------------')
@@ -24,27 +29,27 @@ def print_header():
 
 
 def get_folder_from_user():
-    folder = input('What folder do you want to search?')
+    folder = input('What folder do you want to search? ')
     if not folder or not folder.strip():
         return None
 
     if not os.path.isdir(folder):
         return None
 
-    return os.oath.abspath(folder)
+    return os.path.abspath(folder)
 
 
 def get_search_text_from_user():
-    text = input('What are you searching for [single phrases only]?')
-    return text
+    text = input('What are you searching for [single phrases only]? ')
+    return text.lower()
 
 
-def search_folders():
-    print("Searching {} for {}".format(folder, text)
+def search_folders(folder, text):
+    print("Searching {} for {}".format(folder, text))
 
     all_matches = []
 
-    items=os.listdir(folder)
+    items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
@@ -56,7 +61,17 @@ def search_folders():
 
     return matches
 
+def search_file(filename, search_text):
 
+    matches = []
+
+    with open(filename,'r', encoding='utf-8') as fin:
+
+        for line in fin:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+        return matches
 
 if __name__ == '__main__':
     main()
