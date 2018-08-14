eafad410b0bba9146a2417c709e1491bb550e45e
Elvin Ramirez <real8686@gmail.com>
2018-08-13 22:02:30 -0400

    improve on search results

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 import os
+import collections
 
+SearchResult = collections.namedtuple('SearchResult',
+                                      'file, line, text')
 
 def main():
     print_header()
@@ -19,7 +22,11 @@ def main():
     matches = search_folders(folder, text)
 
     for m in matches:
-        print(m)
+        print('-------- Matches ---------')
+        print('file: ' + m.file)
+        print('line: {}'.format(m.line))
+        print('match: ' + m.text)
+        print()
 
 
 def print_header():
@@ -68,10 +75,12 @@ def search_file(filename, search_text):
     matches = []
 
     with open(filename, 'r', encoding='utf-8') as fin:
-
+        line_num = 0
         for line in fin:
+            line_num += 1
             if line.lower().find(search_text) >= 0:
-                matches.append(line)
+                m = SearchResult(line=line_num,file=filename,text=line)
+                matches.append(m)
 
         return matches
 
